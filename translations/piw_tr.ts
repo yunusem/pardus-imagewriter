d60a84d70e5ed939b836f56c459a329903c27994
Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>
2018-12-20 15:47:52 +0300

    Update Turkish translations
    Signed-off-by: Yunusemre Şentürk <yunusemre.senturk@pardus.org.tr>

--- translations/piw_tr.ts
+++ translations/piw_tr.ts
@@ -4,12 +4,22 @@
 <context>
     <name>Burn</name>
     <message>
-        <location filename="../ui/Burn.qml" line="105"/>
+        <location filename="../ui/Burn.qml" line="48"/>
+        <source>Click to return disk image selection</source>
+        <translation>Disk kalıbı seçim sayfası için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/Burn.qml" line="108"/>
+        <source>Click to return target selection</source>
+        <translation>Hedef seçim sayfası için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/Burn.qml" line="155"/>
         <source>Start</source>
         <translation>Başlat</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="110"/>
+        <location filename="../ui/Burn.qml" line="160"/>
         <source>This operation will erase all
 the data in your target device.
 
@@ -20,7 +30,7 @@ bütün verileri silecektir.
 Devam etmek istiyor musunuz ?</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="113"/>
+        <location filename="../ui/Burn.qml" line="163"/>
         <source>Writing process could not start.
 
 The disk image size is greater
@@ -31,28 +41,33 @@ Disk kalıbı boyutu hedef
 cihazın boyutundan daha yüksek.</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="164"/>
+        <location filename="../ui/Burn.qml" line="169"/>
+        <source>Click to start burning</source>
+        <translation>Yakmayı başlatmak için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/Burn.qml" line="213"/>
         <source>CANCELLING...</source>
         <translation>İPTAL EDİLİYOR...</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="164"/>
+        <location filename="../ui/Burn.qml" line="213"/>
         <source>Cancel</source>
         <translation>İptal Et</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="169"/>
+        <location filename="../ui/Burn.qml" line="218"/>
         <source>Are you sure to cancel ?</source>
         <translation>İptal etmek istediğinize emin misiniz ?</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="193"/>
-        <location filename="../ui/Burn.qml" line="201"/>
+        <location filename="../ui/Burn.qml" line="242"/>
+        <location filename="../ui/Burn.qml" line="250"/>
         <source>Pardus Image Writer</source>
         <translation>Pardus Disk Kalıbı Yazıcı</translation>
     </message>
     <message>
-        <location filename="../ui/Burn.qml" line="195"/>
+        <location filename="../ui/Burn.qml" line="244"/>
         <source>Writing process is Finished !</source>
         <translation>Yazma işlemi Tamamlandı !</translation>
     </message>
@@ -65,12 +80,17 @@ cihazın boyutundan daha yüksek.</translation>
         <translation>Disk kalıbı dosyasını seçiniz</translation>
     </message>
     <message>
-        <location filename="../ui/File.qml" line="64"/>
+        <location filename="../ui/File.qml" line="39"/>
+        <source>Click to choose disk image</source>
+        <translation>Disk kalıbı seçmek için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/File.qml" line="80"/>
         <source>Please choose the disk image</source>
         <translation>Lütfen disk kalıbını seçiniz</translation>
     </message>
     <message>
-        <location filename="../ui/File.qml" line="66"/>
+        <location filename="../ui/File.qml" line="82"/>
         <source>Disk images</source>
         <translation>Disk Kalıpları</translation>
     </message>
@@ -94,74 +114,94 @@ cihazın boyutundan daha yüksek.</translation>
 <context>
     <name>main</name>
     <message>
-        <location filename="../ui/main.qml" line="14"/>
-        <location filename="../ui/main.qml" line="242"/>
+        <location filename="../ui/main.qml" line="15"/>
+        <location filename="../ui/main.qml" line="288"/>
         <source>Pardus Image Writer</source>
         <translation>Pardus Disk Kalıbı Yazıcı</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="33"/>
+        <location filename="../ui/main.qml" line="34"/>
         <source>Something is trying to kill me!!!</source>
         <translation>Bir şey beni öldürmeye çalışıyor !!!</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="129"/>
+        <location filename="../ui/main.qml" line="133"/>
         <source>NEXT</source>
         <translation>İLERİ</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="147"/>
+        <location filename="../ui/main.qml" line="139"/>
+        <source>Click to move forward</source>
+        <translation>İlerlemek için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/main.qml" line="158"/>
         <source>BACK</source>
         <translation>GERİ</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="169"/>
-        <location filename="../ui/main.qml" line="449"/>
+        <location filename="../ui/main.qml" line="164"/>
+        <source>Click to move backward</source>
+        <translation>Geri gitmek için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/main.qml" line="195"/>
+        <location filename="../ui/main.qml" line="503"/>
         <source>Are you sure to exit ?</source>
         <translation>Çıkmak istediğinize emin misiniz ?</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="215"/>
+        <location filename="../ui/main.qml" line="199"/>
+        <source>Click to close the application</source>
+        <translation>Uygulamayı kapatmak için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/main.qml" line="231"/>
+        <source>Click to minimize</source>
+        <translation>Küçültmek için tıklayın</translation>
+    </message>
+    <message>
+        <location filename="../ui/main.qml" line="261"/>
         <source>About</source>
         <translation>Hakkında</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="255"/>
+        <location filename="../ui/main.qml" line="301"/>
         <source>Author</source>
         <translation>Yazar</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="261"/>
+        <location filename="../ui/main.qml" line="307"/>
         <source>Source Code</source>
         <translation>Kaynak Kodu</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="274"/>
+        <location filename="../ui/main.qml" line="320"/>
         <source>License</source>
         <translation>Lisans</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="288"/>
+        <location filename="../ui/main.qml" line="334"/>
         <source>Release</source>
         <translation>Sürüm</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="300"/>
+        <location filename="../ui/main.qml" line="348"/>
         <source>Ok</source>
         <translation>Tamam</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="370"/>
+        <location filename="../ui/main.qml" line="428"/>
         <source>Yes</source>
         <translation>Evet</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="382"/>
+        <location filename="../ui/main.qml" line="439"/>
         <source>No</source>
         <translation>Hayır</translation>
     </message>
     <message>
-        <location filename="../ui/main.qml" line="447"/>
+        <location filename="../ui/main.qml" line="501"/>
         <source>Writing process is ongoing.
 Terminating it is not recommended.
 
